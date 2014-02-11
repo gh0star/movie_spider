22a3e45d193d9bf683f644f41c7f015bc6b5f3f6
lxstar <362377891@qq.com>
2014-02-11 08:44:45 +0000

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import time
+
+# DEBUG 
+DEBUG = False
+
+# Security Key
+KEY = "lxstar362377891"
+
+# Mail Sender Address
+MAIL_SENDER = "lxstar01@gmail.com"
+
+# Mail To Address
+MAIL_TO = "362377891@qq.com;837274783@qq.com;529380446@qq.com"
+
+# Mail Subject
+MAIL_SUBJECT = "lxstar 电影爬虫 %s"%time.strftime('%Y-%m-%d',time.localtime(time.time()))
+
+# Debug Mail Address
+DEBUG_MAIL_TO = "362377891@qq.com"
+
+# Fetch WebSite
+FETCH_SITE = "http://goxiazai.cc/"
